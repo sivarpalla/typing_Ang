856c290c7ecc8866fad038a189f9df46e404e027
sivarpalla <80324556+sivarpalla@users.noreply.github.com>
2024-08-30 07:32:13 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { lorem } from 'faker';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
   title = 'typing';
+  randomText = lorem.sentence();
+  enteredText:string = '';
+  enableSuccess:boolean=false;
+
+  onInput(event: any) {
+    this.enteredText = event.target.value;
+    if(this.enteredText === this.randomText){
+      this.enableSuccess=true;
+    }
+  }
+
+  compare(randomLetter: string, enteredLetter: string) {
+      if(!enteredLetter){
+        return 'pending';
+      }
+
+      return randomLetter === enteredLetter ? 'correct':'incorrect';
+    }
+    
 }
